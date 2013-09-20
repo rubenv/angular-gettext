7c580319fd08872adead1110947d45f1ea7fa7e0
Ruben Vermeersch <ruben@savanne.be>
2013-09-20 10:12:50 +0200

    Check for the presence of jQuery.

--- dist/angular-gettext.js
+++ dist/angular-gettext.js
@@ -1,3 +1,6 @@
+if (typeof jQuery === 'undefined') {
+  throw new Error('Angular-gettext depends on jQuery, be sure to include it!');
+}
 angular.module('gettext', []);
 angular.module('gettext').factory('gettext', function () {
   return function (str) {
--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+if (typeof jQuery === 'undefined') { 
+    throw new Error('Angular-gettext depends on jQuery, be sure to include it!');
+}
+
 angular.module('gettext', []);
 
 angular.module('gettext').factory('gettext', function () {
