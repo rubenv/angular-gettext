22ee9031e9ab8fc9f3ece3e3c4b364f08fbd8325
Ivan Mitrić <ivan.mitric@fleka.me>
2013-10-12 14:59:51 +0200

    Add Montenegrin language

--- dist/angular-gettext.js
+++ dist/angular-gettext.js
@@ -167,6 +167,7 @@ angular.module('gettext').factory('gettextPlurals', function () {
     case 'bs':
     case 'hr':
     case 'ru':
+    case 'me':
     case 'sr':
     case 'uk':
       return n % 10 == 1 && n % 100 != 11 ? 0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2;
