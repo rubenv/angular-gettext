2ae21ec16bd52dc180fdbc913e44854e519fe744
Maxim Mitroshin <rocco66max@gmail.com>
2013-11-08 16:07:50 +0400

    Add C style interpolation and tests

--- src/filter.js
+++ src/filter.js
@@ -1,5 +1,7 @@
 angular.module('gettext').filter('translate', function (gettextCatalog, $interpolate, $parse) {
     return function (input) {
-        return gettextCatalog.getString(input);
+        var translation = gettextCatalog.getString(input),
+            argumentsForSprintf = Array.prototype.slice.call(arguments, 1);
+        return vsprintf(translation, argumentsForSprintf);
     };
 });
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-if (typeof jQuery === 'undefined') { 
+if (typeof jQuery === 'undefined') {
     throw new Error('Angular-gettext depends on jQuery, be sure to include it!');
+}
+if (typeof sprintf === 'undefined') {
+    throw new Error('Angular-gettext depends on sprintf, be sure to include it!');
 }
 
 angular.module('gettext', []);
@@ -7,7 +10,7 @@ angular.module('gettext', []);
 angular.module('gettext').factory('gettext', function () {
     /*
      * Does nothing, simply returns the input string.
-     * 
+     *
      * This function serves as a marker for `grunt-angular-gettext` to know that
      * this string should be extracted for translations.
      */
