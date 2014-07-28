86aa3493760ff5a05e5e7f9d293bf0409a67f2c0
Ruben Vermeersch <ruben@rocketeer.be>
2014-07-28 12:07:22 +0200

    Add a test case for #74.

---
+++ test/e2e/xss.js
@@ -0,0 +1,20 @@
+var tests = {
+    "Should not allow XSS": function (file) {
+        return function () {
+            browser().navigateTo("/test/fixtures/xss" + file + ".html");
+            expect(element("body").text()).toBe("fail");
+        };
+    }
+};
+
+describe("XSS", function () {
+    for (var key in tests) {
+        it(key, tests[key](""));
+    }
+});
+
+describe("XSS (no jQuery)", function () {
+    for (var key in tests) {
+        it(key, tests[key]("-nojquery"));
+    }
+});
