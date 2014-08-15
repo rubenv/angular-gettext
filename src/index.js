7b6a2502ef6d83dd2818edf18195dc98fb70a128
Rickert Mulder <circlingthesun@gmail.com>
2014-08-15 05:32:48 +0200

    Makes gettext() work as expected

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 angular.module('gettext', []);
 
-angular.module('gettext').constant('gettext', function (str) {
-    /*
-     * Does nothing, simply returns the input string.
-     *
-     * This function serves as a marker for `grunt-angular-gettext` to know that
-     * this string should be extracted for translations.
-     */
-    return str;
-});
+angular.module('gettext').factory('gettext', ['gettextCatalog', function (gettextCatalog) {
+    return function (str) {
+        return gettextCatalog.getString(str);
+    };
+}]);
