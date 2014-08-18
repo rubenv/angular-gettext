3febe9f01976fcdf64121cee2cdaf2257c18a263
Rickert Mulder <circlingthesun@gmail.com>
2014-08-18 17:44:18 +0200

    Revert unit test modification

--- Gruntfile.js
+++ Gruntfile.js
@@ -93,7 +93,7 @@ module.exports = function (grunt) {
             unit: {
                 configFile: "test/configs/unit.conf.js",
                 browsers: ["PhantomJS"],
-                background: false
+                background: true
             },
             unit_nojquery: {
                 configFile: "test/configs/unit-nojquery.conf.js",
