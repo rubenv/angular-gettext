9b0fff9ba67b95f3f26bb633e37c6d790b57f398
Ruben Vermeersch <ruben@rocketeer.be>
2014-08-22 11:05:24 +0200

    Reformat.

--- src/directive.js
+++ src/directive.js
@@ -1,70 +1,68 @@
-angular.module('gettext')
-    // First directive substitutes the correct translation
-    .directive('translate', function (gettextCatalog, $parse, $animate, $compile) {
-        // Trim polyfill for old browsers (instead of jQuery)
-        // Based on AngularJS-v1.2.2 (angular.js#620)
-        var trim = (function () {
-            if (!String.prototype.trim) {
-                return function (value) {
-                    return (typeof value === 'string') ? value.replace(/^\s*/, '').replace(/\s*$/, '') : value;
-                };
-            }
+angular.module('gettext').directive('translate', function (gettextCatalog, $parse, $animate, $compile) {
+    // Trim polyfill for old browsers (instead of jQuery)
+    // Based on AngularJS-v1.2.2 (angular.js#620)
+    var trim = (function () {
+        if (!String.prototype.trim) {
             return function (value) {
-                return (typeof value === 'string') ? value.trim() : value;
+                return (typeof value === 'string') ? value.replace(/^\s*/, '').replace(/\s*$/, '') : value;
             };
-        })();
-
-        function assert(condition, missing, found) {
-            if (!condition) {
-                throw new Error('You should add a ' + missing + ' attribute whenever you add a ' + found + ' attribute.');
-            }
         }
-
-        return {
-            restrict: 'A',
-            terminal: true,
-            compile: function compile(element, attrs) {
-                // Validate attributes
-                assert(!attrs.translatePlural || attrs.translateN, 'translate-n', 'translate-plural');
-                assert(!attrs.translateN || attrs.translatePlural, 'translate-plural', 'translate-n');
-
-                var msgid = trim(element.html());
-                var translatePlural = attrs.translatePlural;
-
-                return {
-                    post: function (scope, element, attrs) {
-                        var countFn = $parse(attrs.translateN);
-                        var pluralScope = null;
-
-                        function update() {
-                            // Fetch correct translated string.
-                            var translated;
-                            if (translatePlural) {
-                                scope = pluralScope || (pluralScope = scope.$new());
-                                scope.$count = countFn(scope);
-                                translated = gettextCatalog.getPlural(scope.$count, msgid, translatePlural);
-                            } else {
-                                translated = gettextCatalog.getString(msgid);
-                            }
-
-                            // Swap in the translation
-                            var newWrapper = angular.element('<span>' + translated + '</span>');
-                            $compile(newWrapper.contents())(scope);
-                            var oldContents = element.contents();
-                            var newContents = newWrapper.contents();
-                            $animate.enter(newContents, element);
-                            $animate.leave(oldContents);
-                        }
-
-                        if (attrs.translateN) {
-                            scope.$watch(attrs.translateN, update);
-                        }
-
-                        scope.$on('gettextLanguageChanged', update);
-
-                        update();
-                    }
-                };
-            }
+        return function (value) {
+            return (typeof value === 'string') ? value.trim() : value;
         };
-    });
+    })();
+
+    function assert(condition, missing, found) {
+        if (!condition) {
+            throw new Error('You should add a ' + missing + ' attribute whenever you add a ' + found + ' attribute.');
+        }
+    }
+
+    return {
+        restrict: 'A',
+        terminal: true,
+        compile: function compile(element, attrs) {
+            // Validate attributes
+            assert(!attrs.translatePlural || attrs.translateN, 'translate-n', 'translate-plural');
+            assert(!attrs.translateN || attrs.translatePlural, 'translate-plural', 'translate-n');
+
+            var msgid = trim(element.html());
+            var translatePlural = attrs.translatePlural;
+
+            return {
+                post: function (scope, element, attrs) {
+                    var countFn = $parse(attrs.translateN);
+                    var pluralScope = null;
+
+                    function update() {
+                        // Fetch correct translated string.
+                        var translated;
+                        if (translatePlural) {
+                            scope = pluralScope || (pluralScope = scope.$new());
+                            scope.$count = countFn(scope);
+                            translated = gettextCatalog.getPlural(scope.$count, msgid, translatePlural);
+                        } else {
+                            translated = gettextCatalog.getString(msgid);
+                        }
+
+                        // Swap in the translation
+                        var newWrapper = angular.element('<span>' + translated + '</span>');
+                        $compile(newWrapper.contents())(scope);
+                        var oldContents = element.contents();
+                        var newContents = newWrapper.contents();
+                        $animate.enter(newContents, element);
+                        $animate.leave(oldContents);
+                    }
+
+                    if (attrs.translateN) {
+                        scope.$watch(attrs.translateN, update);
+                    }
+
+                    scope.$on('gettextLanguageChanged', update);
+
+                    update();
+                }
+            };
+        }
+    };
+});
