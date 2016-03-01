b16427965a3905439c6105f0fe224d63def9807a
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2016-03-01 15:46:10 +0000

    #262 - dist not minimised updated

--- dist/angular-gettext.js
+++ dist/angular-gettext.js
@@ -66,13 +66,6 @@ angular.module('gettext').factory('gettextCatalog', ["gettextPlurals", "gettextF
     var catalog;
     var noContext = '$$noContext';
 
-    // IE8 returns UPPER CASE tags, even though the source is lower case.
-    // This can causes the (key) string in the DOM to have a different case to
-    // the string in the `po` files.
-    // IE9, IE10 and IE11 reorders the attributes of tags.
-    var test = '<span id="test" title="test" class="tested">test</span>';
-    var isHTMLModified = (angular.element('<span>' + test + '</span>').html() !== test);
-
     var prefixDebug = function (string) {
         if (catalog.debug && catalog.currentLanguage !== catalog.baseLanguage) {
             return catalog.debugPrefix + string;
@@ -219,11 +212,6 @@ angular.module('gettext').factory('gettextCatalog', ["gettextPlurals", "gettextF
 
             for (var key in strings) {
                 var val = strings[key];
-
-                if (isHTMLModified) {
-                    // Use the DOM engine to render any HTML in the key (#131).
-                    key = angular.element('<span>' + key + '</span>').html();
-                }
 
                 if (angular.isString(val) || angular.isArray(val)) {
                     // No context, wrap it in $$noContext.
