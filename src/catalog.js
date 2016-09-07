0fd91a8c84156c09a20c0c28303a42cf9df664cc
Patrick Sullivan <sullivanpt@acm.org>
2016-09-07 12:38:44 -0700

    fix: arabic (ar) default plural for 1 maps correctly

--- src/catalog.js
+++ src/catalog.js
@@ -183,7 +183,10 @@ angular.module('gettext').factory('gettextCatalog', function (gettextPlurals, ge
                 // Expand single strings for each context.
                 for (var context in val) {
                     var str = val[context];
-                    val[context] = angular.isArray(str) ? str : [str];
+                    if (!angular.isArray(str)) {
+                        val[context] = [];
+                        val[context][gettextPlurals(language, 1)] = str;
+                    }
                 }
                 this.strings[language][key] = val;
             }
