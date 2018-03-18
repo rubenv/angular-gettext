53cfc717c71dc8fdf8c41c733a96f8a67ea97460
Richard DesLonde <rdeslonde@deslondesoftware.com>
2018-03-18 00:33:34 -0700

    Fixed jshint errors

--- src/catalog.js
+++ src/catalog.js
@@ -47,16 +47,16 @@ angular.module('gettext').factory('gettextCatalog', function (gettextPlurals, ge
         $rootScope.$broadcast('gettextLanguageChanged');
     }
 
-    function rollBackOvercorrections (originalKey, key) {
+    function rollBackOvercorrections(originalKey, key) {
         var originalKeyParts = originalKey.split('&');
-        var re = /&amp;/gi; //replacement match regex
+        var re = /&amp;/gi;//replacement match regex
 
         var nth = 0;
         var i = 0;
-        
-        function replaceNth (match) {
+
+        function replaceNth(match) {
             nth++;
-            if(nth === i+1) {
+            if (nth === i + 1) {
                 return '&';
             }
             return match;
@@ -64,9 +64,9 @@ angular.module('gettext').factory('gettextCatalog', function (gettextPlurals, ge
 
         for (i = 0; i < originalKeyParts.length; i++) {
             //get the next part, which will start with "amp;" if this was an occurrence of an encoded &
-            var nextPart = (originalKeyParts.length-1 >= i+1) ? originalKeyParts[i+1] : "";
+            var nextPart = (originalKeyParts.length-1 >= i + 1) ? originalKeyParts[i + 1] : '';
 
-            if (nextPart.length < 4 || nextPart.substring(0, 5) !== "amp;") { //unencoded & in original, needs to be rolled back in key
+            if (nextPart.length < 4 || nextPart.substring(0, 5) !== 'amp;') { //unencoded & in original, needs to be rolled back in key
                 key = key.replace(re, replaceNth);
             }
         }
